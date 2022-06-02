bc98395751b0d88472febeafb51f7d85b0e42b02
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-02 11:41:05 +0200

    setup nevigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import SideBar from "./components/common/sidebar";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
+        <SideBar />
         <Movies />
       </main>
     );
